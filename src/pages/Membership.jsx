73aeac5fa641fb1709af15d7ef88fdb87eda1fa1
Joshua Vine <joshuavine54@gmail.com>
2025-03-29 19:28:48 +0400

    Membership Page

--- src/pages/Membership.jsx
+++ src/pages/Membership.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import Header from "../components/Header"
 import Footer from '../components/Footer';
+import Pricing from '../components/Pricing';
+import Faq from '../components/Faq';
+import {default as Testimonial} from "../components/Testimonial"
+
 const Membership = () => {
   return (
     <>
+        <Header />
+        <section className="padding bg-black font-montserrat">
+        <h2 className="text-5xl text-customYellow mb-6 font-semibold text-center">Join the club</h2>
+        <p className="text-white text-center">Be part of an exclusive community with access to premium features, insightful resources, and personalized experiences designed to elevate your journey. Choose a plan that fits your goals and enjoy the perks of being a member!</p>
+        </section>
+        <Pricing />
+        <Faq/>
+        <Testimonial />
+        <Footer />
+
         
     </>
   )
